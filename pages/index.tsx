d5009eedc7f09c879076f628eab5892ff7f6b6c1
Balázs Orbán <info@balazsorban.com>
2022-01-22 16:45:39 +0100

    foo

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,15 +19,7 @@ export default function Home({ data }: { data: DayData[] }) {
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+        <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
       </footer>
     </div>
   )
