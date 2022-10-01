a32e6b32dcb637bab577bc826bc6686e5da87555
Balázs Orbán <info@balazsorban.com>
2022-10-01 17:28:05 +0200

    simplify type

--- types.ts
+++ types.ts
@@ -1,9 +1,8 @@
 import type { Day } from "@prisma/client"
-import type { Octokit } from "octokit"
 export type DayData = Pick<Day, "date" | "totalOpened">
 
-type UnPromisify<T> = T extends Promise<infer U> ? U : T
-
-export type Issue = UnPromisify<
-  ReturnType<Octokit["rest"]["issues"]["listForRepo"]>
->["data"][0]
+export interface Issue {
+  merged_at?: string | null
+  closed_at?: string | null
+  created_at: string
+}
